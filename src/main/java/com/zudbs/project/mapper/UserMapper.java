75ddd35d64be7246f7a10f75137c6baec5c28d7e
Gyeongyun Kim <zudbs1@gmail.com>
2020-05-06 19:49:28 +0900

    [#2] 회원 가입 기능
    @Controller 어노테이션 설명 추가
    @Mapper 어노테이션 설명 추가

--- src/main/java/com/zudbs/project/mapper/UserMapper.java
+++ src/main/java/com/zudbs/project/mapper/UserMapper.java
@@ -3,7 +3,13 @@ package com.zudbs.project.mapper;
 import com.zudbs.project.model.User;
 import org.apache.ibatis.annotations.Mapper;
 
-@Mapper /* My-batis Mapper로 등록 */
+@Mapper
+/*
+Mybatis가 DB에 관한 DataSource 정보를 토대로 SQL문이 정의된 Mapper.xml객체를 생성할 때
+해당 어노테이션 또는 Mapper Interface를 Implements해서 객체를 생성하여 Mapper로 등록합니다.
+이렇게 만들어지는 Mapper에는 Mapper.xml에서 작성한 SQL 문을 명시한 DB에서 실행하는 메소드들이
+구현되어 있습니다.
+*/
 public interface UserMapper {
     void  insertUser(User user);
 }
