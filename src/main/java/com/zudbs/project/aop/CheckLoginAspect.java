48cad6fb4d81961686bacca41f5f23fcca2b3399
Gyeongyun Kim <zudbs1@gmail.com>
2020-09-04 15:19:47 +0900

    [#20] 로그인 검사 로직 분리
    - HttpSession 가져오는 로직 수정

--- src/main/java/com/zudbs/project/aop/CheckLoginAspect.java
+++ src/main/java/com/zudbs/project/aop/CheckLoginAspect.java
@@ -1,28 +1,37 @@
 package com.zudbs.project.aop;
 
 import com.zudbs.project.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
-import org.aspectj.lang.JoinPoint ;
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.web.client.HttpStatusCodeException;
-import org.springframework.web.context.request.RequestContextHolder;
-import org.springframework.web.context.request.ServletRequestAttributes;
+
 import javax.servlet.http.HttpSession;
 
-@Component // @Aspect 만으로는 빈으로 등록되지 않기때문에 빈으로 등록하기 위한 어노테이션
-@Aspect // 해당 클래스가 부가기능을 담당하는 클래스라는 것을 나타내는 어노테이션
+@Component
+/*
+@Aspect 어노테이션은 Aspectj를 사용하는 클래스를 명시해주는 것이며
+Spring Framework에 종속되지 않은 클래스이므로 빈으로 자동 등록되지 않는다.
+따라서 Spring Aop를 이용하기 위해서는 빈으로 따로 등록하는 과정이 필요하다.
+*/
+
+@Aspect
+/*해당 클래스가 부가기능을 담당하는 클래스라는 것을 나타내는 어노테이션*/
 public class CheckLoginAspect {
 
-    @Before("@annotation(com.zudbs.project.annotation.CheckLogin)")
-    public void checkLogin(JoinPoint  joinPoint) throws Throwable {
+    @Autowired
+    private HttpSession session;
 
-        HttpSession session = ((ServletRequestAttributes)(RequestContextHolder.currentRequestAttributes())).getRequest().getSession();
-        User user= (User) session.getAttribute("user");
+    @Before("@annotation(com.zudbs.project.annotation.CheckLogin)")
+    public void checkLogin(JoinPoint joinPoint) throws Throwable {
+
+        User user = (User) session.getAttribute("user");
 
         if (user == null) {
-            throw new HttpStatusCodeException(HttpStatus.UNAUTHORIZED, "NO_LOGIN") {};
+            throw new HttpStatusCodeException(HttpStatus.UNAUTHORIZED){};
         }
     }
 }
