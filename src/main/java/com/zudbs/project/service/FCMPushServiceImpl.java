0be72bdf07d39cb5236d5bedc9021a2d6fe78453
Gyeongyun Kim <zudbs1@gmail.com>
2021-02-17 22:22:19 +0900

    [#19] 알림기능 구현
    - ConcurrentHashMap 관련 설명 주석 추가

--- src/main/java/com/zudbs/project/service/FCMPushServiceImpl.java
+++ src/main/java/com/zudbs/project/service/FCMPushServiceImpl.java
@@ -25,6 +25,20 @@ public class FCMPushServiceImpl implements PushMessageService {
     @Value("${firebaseConfigPath}")
     private String firebaseConfigPath;
 
+    /*
+    ConcurrentHashMap
+    Multi-Thread 환경을 지원하는 Map이다.
+    HashTable과 달리 메소드 단위로 잠금을 수행하는 것이 아닌
+    Concurrency 레벨에 기반하여 map을 여러 파트로 나누어 두고,
+    업데이트 동작이 일어나는 동안 오직 그 부분에만 잠금을 수행한다.
+
+    HashMap -> Thread-safe X,
+
+    HashTable -> Thread-safe O
+    (put,get 메서드를 synchronizeded 하게 구현하여 멀티 스레드를 지원한다.)
+   */
+    private Map<String, String> tokenMap = new ConcurrentHashMap<>();
+
     @PostConstruct //의존성 주입 후 초기화를 수행하는 메서드
     public void initialize() throws IOException {
 
@@ -36,6 +50,15 @@ public class FCMPushServiceImpl implements PushMessageService {
 
         FirebaseApp.initializeApp(options);
 
+    }
+
+    public void registerReceiver(String userId, String receiver) {
+
+        tokenMap.put(userId, receiver);
+    }
+
+    public void removeReceiver(String userId) {
+        tokenMap.remove(userId);
     }
 
     public void sendPushMessage(String reciver, String title, String content) {
