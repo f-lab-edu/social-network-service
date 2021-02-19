e3a540ed440552dbb46ad376b27621a4c6e58553
Gyeongyun Kim <zudbs1@gmail.com>
2021-02-19 20:50:58 +0900

    [#44] 피드 좋아요 기능 테스트 작성
    - 테스트 클래스 생성

---
+++ src/test/java/com/zudbs/project/FeedLikeTest.java
@@ -0,0 +1,18 @@
+package com.zudbs.project;
+
+import com.zudbs.project.model.FeedLike;
+import org.junit.Test;
+
+public class FeedLikeTest {
+
+    @Test
+    public void addLike(){
+
+    }
+
+    @Test
+    public void cancelLike(){
+
+    }
+
+}
