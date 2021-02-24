b9ea4c074a7f3eac6f20d07a4f074c6cacb0cf6f
Gyeongyun Kim <zudbs1@gmail.com>
2021-02-24 22:05:35 +0900

    [#35] 피드 좋아요 기능
    - Feed 좋아요 기능 URL 수정

--- src/main/java/com/zudbs/project/controller/FeedController.java
+++ src/main/java/com/zudbs/project/controller/FeedController.java
@@ -73,7 +73,7 @@ public class FeedController {
     }
 
     @CheckLogin
-    @PostMapping("/likes/{feedId}/likes")
+    @PostMapping("/{feedId}/likes")
     public HttpStatus addFeedLike(@SessionVariable(SessionKey.LOGIN_USER_ID) String userId, @PathVariable int feedId) {
 
         feedService.addFeedLike( new FeedLike(feedId,userId));
@@ -82,7 +82,7 @@ public class FeedController {
     }
 
     @CheckLogin
-    @DeleteMapping("/likes/{feedId}/likes")
+    @DeleteMapping("/{feedId}/likes")
     public HttpStatus cancelFeedLike(@SessionVariable(SessionKey.LOGIN_USER_ID) String userId, @PathVariable int feedId) {
 
         feedService.cancelFeedLike(new FeedLike(feedId,userId));
